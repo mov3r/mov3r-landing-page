0dca33d6c4da710a2dc7eac959e19e9192cba813
ilya-bkv <i.yablokov@ccmp.me>
2022-08-22 08:32:42 +0300

    integrate API

--- src/components/WaitlistForm/WaitlistForm.tsx
+++ src/components/WaitlistForm/WaitlistForm.tsx
@@ -6,6 +6,7 @@ import axios from 'axios';
 import {useDispatch} from 'react-redux';
 import {isEmailSent} from '../../store/userSlice';
 import styles from './WaitlistForm.module.scss'
+import HCaptcha from '@hcaptcha/react-hcaptcha';
 
 type WaitlistFormProps = {
   className?: string
@@ -18,6 +19,8 @@ const isValidEmail = (email:string) => {
 const WaitlistForm: React.FC<WaitlistFormProps> = (props) => {
   const [email, setEmail] = React.useState<string>('')
   const [error, setError] = React.useState<string | undefined>(undefined)
+  const [token, setToken] = React.useState<string | undefined>(undefined);
+  const captcha = React.useRef<HCaptcha>(null);
   const dispatch = useDispatch();
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setError(undefined)
@@ -28,16 +31,23 @@ const WaitlistForm: React.FC<WaitlistFormProps> = (props) => {
       setError('Email is invalid')
       return
     }
+    if (!token) {
+      setError("You must verify the captcha");
+      return;
+    }
     axios.post(`${process.env.REACT_APP_API_URL}/waitlist/add/`, {
+      hcaptcha: token,
       email: email
     }).then(() => {
       dispatch(isEmailSent(true))
     }).catch(function (error) {
       setError(error.response.data.error ? error.response.data.error : error.message)
       dispatch(isEmailSent(false))
+    }).finally(() => {
+      if (captcha.current) captcha.current.resetCaptcha();
+      setToken(undefined);
     })
   }
-
 
   return (
     <div className={cn(styles.wrapper, props.className)}>
@@ -51,10 +61,19 @@ const WaitlistForm: React.FC<WaitlistFormProps> = (props) => {
           onChange={handleInputChange}
         />
         <Button
-          disabled={!email || Boolean(error)}
+          disabled={!email || Boolean(error) || !token}
           onClick={handleFormSubmit}>
           Join Waitlist <ArrowIcon className={styles.arrowIcon}/>
         </Button>
+      </div>
+      <div className={styles.hcaptcha}>
+        <HCaptcha
+          ref={captcha}
+          theme="dark"
+          sitekey={`${process.env.REACT_APP_RECAPTCHA_HEY}`}
+          onVerify={token => setToken(token)}
+          onExpire={() => setToken(undefined)}
+        />
       </div>
       <div className={styles.messageWrapper}>
         {error && <div className={styles.errorMessage}>{error}</div>}
