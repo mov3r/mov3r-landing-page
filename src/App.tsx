85b9744b4184fa53f9c7cc77fc62b276de939c4f
ilya-bkv <i.yablokov@ccmp.me>
2022-10-02 22:31:54 +0300

    update labding

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { BrowserRouter as Router } from "react-router-dom"
-import { Background } from './components/Background/Background'
 import HomeScreen from './components/HomeScreen'
 import './App.scss'
 
@@ -9,7 +8,6 @@ function App() {
     return (
     <Router>
       <div className="App">
-        <Background />
         <div className="content">
           <HomeScreen />
         </div>
--- src/components/HomeScreen/HomeScreen.tsx
+++ src/components/HomeScreen/HomeScreen.tsx
@@ -10,118 +10,17 @@ import axios from 'axios';
 import styles from './HomeScreen.module.scss'
 import CopyReferralLink from '../CopyReferralLink';
 import Spinner from '../Spinner';
+import {ReactComponent as Arrow} from '../../assets/Arrow_Left_MD.svg';
 
 const HomeScreen: React.FC = () => {
-  const dispatch = useDispatch();
-  const [referrer, setReferrer] = React.useState<string | undefined>(undefined)
-  // const [urlPaths, setUrlPaths] = React.useState<string[] | undefined>(undefined)
-  const isEmailSent = useSelector((state: User) => state.user.isEmailSent)
-  const rank = useSelector((state: User) => state.user.rank)
-  const loading = useSelector((state: Service) => state.service.loading)
-  const referralLink = useSelector((state: User) => state.user.referralLink)
-  const error = useSelector((state: Service) => state.service.error)
-  const url = new URL(window.location.href);
-
-  React.useEffect(() => {
-    const urlPaths = url.pathname.split('/').splice(1)
-    switch (true) {
-      case (urlPaths[0] === 'c'):
-        dispatch(setSlug(urlPaths[1]))
-        dispatch(setSecret(urlPaths[2]))
-        axios.post(`${process.env.REACT_APP_API_URL}/waitlist/verify/`, {
-          slug: urlPaths[1],
-          secret: urlPaths[2]
-        }).then((response) => {
-          dispatch(setConfirmed(true))
-          dispatch(setRank(response.data.position))
-          dispatch(setReferralLink(response.data.reflink))
-          dispatch(setLoading(false))
-          return
-        }).catch((error) => {
-          if (error.response.data.error_code === 6) { //Already confirmed
-            dispatch(setLinkType('waitlist'))
-            axios.get(`${process.env.REACT_APP_API_URL}/waitlist/position/`, {
-              params: { slug: urlPaths[1] },
-            }).then((response) => {
-              dispatch(setRank(response.data.position))
-              dispatch(setReferralLink(response.data.reflink))
-              window.history.pushState({}, 'Mover', `${url.origin}/w/${urlPaths[1]}/`);
-              dispatch(setLoading(false))
-            })
-            return;
-          }
-          dispatch(setError(error.response.data.error))
-          return
-        })
-        break;
-      case urlPaths[0] === 'r':
-        dispatch(setLinkType('referral'))
-        setReferrer(urlPaths[1])
-        dispatch(setLoading(false))
-        break;
-      case urlPaths[0] === 'w':
-        axios.get(`${process.env.REACT_APP_API_URL}/waitlist/position/`, {
-          params: {slug: urlPaths[1]},
-        }).then((response) => {
-          dispatch(setRank(response.data.position))
-          dispatch(setReferralLink(response.data.reflink))
-          window.history.pushState({}, 'Mover', `${url.origin}/w/${urlPaths[1]}/`);
-          setLoading(false)
-        }).catch((error) => {
-          console.log('!!! error:', error)
-          dispatch(setLoading(false))
-        })
-        break;
-      default: {
-        dispatch(setLinkType('root'))
-        dispatch(setLoading(false))
-      }
-    }
-  },[])
-
   return (
     <div className={styles.home}>
       <Logo className={styles.logo}/>
-      <div className={styles.container}>
-        {loading ? <Spinner/> : (
-          isEmailSent && !error ? (
-            <>
-              <h1 className={styles.title}>Thanks for signing up!</h1>
-              <p className={styles.text}>
-                A verification email has been sent to you.
-                <br/>Please verify your email to secure your
-                spot on the waitlist.</p>
-            </>
-          ) : (
-            <div>
-              { rank ?
-                <>
-                  <p className={styles.text}>Skip ahead in line by referring friends<br/>using the link below.</p>
-                  <div className={styles.rank}>Your rank: <span>{rank}</span></div>
-                </>
-                :
-                <>
-                  <h1 className={styles.title}>The first Aptos bridge</h1>
-                  <p className={styles.text}>
-                    Join the waitlist to be the first
-                    <br/>
-                    to access our Private Alpha
-                  </p>
-                </>
-              }
-              { referralLink ?
-                <CopyReferralLink className={styles.form}/>
-                :
-                <WaitlistForm
-                  fromReferral={referrer}
-                  error={error}
-                  className={styles.form}
-                />
-              }
-            </div>
-          )
-        )}
-      </div>
+      <h1 className={styles.title}>The first Aptos bridge</h1>
+      <article className={styles.start}>
+        <p>Start enjoying the best bridge <br/> in the world</p>
+        <a href="https://app.mov3r.xyz/" className={styles.startButton}>Use Mover <Arrow className={styles.arrow}/></a>
+      </article>
 
       <Social networks={['discord', 'github', 'twitter']} className={styles.social}/>
     </div>
